6a99ed32f5852fbea47f2175d04ae20683d64826
dhanushss2409-blip <dhanush.ss2409@gmail.com>
2026-06-05 09:04:19 +0530

    Add files via upload

---
+++ lambda learning.py
@@ -0,0 +1,10 @@
+
+nums=[1,2,3,4]
+"""
+print(list(map(lambda x:x*2,nums)))
+
+print(list(filter(lambda x:x%2!=0,nums)))
+"""
+
+words=["hi","python","idle"]
+print(list(map(lambda x:x.upper(),words)))
